79ec60f50a121ed012935dc92a807b2a370a7330
Jose Alvarez C <alvarez.jo.developer@gmail.com>
2018-06-06 00:49:35 -0700

    Created base classes for scriptable objects
    https://unity3d.com/learn/tutorials/topics/user-interface-ui/building-framework

---
+++ Scriptable Objects Tutorial/Assets/Scripts/FlexibleUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode()] // so that we don't have to play the game everytime
+
+public class FlexibleUI : MonoBehaviour {
+
+    // Let's create a reference to the scriptable objects so that we can inherit some values
+    // from it
+    public FlexibleUIData skinData;
+
+    protected virtual void OnSkinUI()
+    {
+        // apparently where the objects will get and set the values retrieved from the skin object
+    }
+
+
+    public virtual void Awake()
+    {
+        OnSkinUI();
+    }
+
+    public virtual void Update()
+    {
+        if (Application.isEditor)
+        {
+            OnSkinUI();
+        }
+    }
+
+}
---
+++ Scriptable Objects Tutorial/Assets/Scripts/FlexibleUIData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Flexible UI Data")]
+// Allows to create instances of this class, as a file or something like it  
+// an  "skin object" / scriptable object 
+
+
+public class FlexibleUIData : ScriptableObject {
+
+
+}
