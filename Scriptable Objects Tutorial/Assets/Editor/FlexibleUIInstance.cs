a8c0d1395adb31b09e84fd42aa6db220729c2dd2
Jose Alvarez C <alvarez.jo.developer@gmail.com>
2018-06-06 19:17:39 -0700

    Wrote an script that allows to instantiate a button based on an scriptable object just by right clicing in the editor
    https://unity3d.com/learn/tutorials/topics/user-interface-ui/menu-shortcuts-and-additional-styling?playlist=17111

---
+++ Scriptable Objects Tutorial/Assets/Editor/FlexibleUIInstance.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+// this script will contain a static method that will allow us to create scriptable objects
+// from right clicking in the editor
+
+public class FlexibleUIInstance : Editor {
+
+    static GameObject clickedObject;
+
+    private static GameObject Create(string objectName)
+    {
+        GameObject instance = Instantiate(Resources.Load<GameObject>(objectName));
+        instance.name = objectName;
+
+        clickedObject = UnityEditor.Selection.activeObject as GameObject;
+
+        if (clickedObject != null)
+        {
+            instance.transform.SetParent(clickedObject.transform, false);
+        }
+
+        return instance;
+    }
+
+
+    [MenuItem("GameObject/Flexible UI/Button Scriptable Object", priority = 0)]
+    public static void AddButton()
+    {
+        Create("Button_ScriptableObject");
+    }
+
+
+}
